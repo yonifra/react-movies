2826733cbc879573f7e44ffbc7df8eb4d12c194a
Jonathan Fraimorice <yonifra@gmail.com>
2020-10-06 00:26:55 +0300

    more info

--- src/Components/movieDetails.js
+++ src/Components/movieDetails.js
@@ -1,4 +1,18 @@
 import React, {useState} from "react";
+import imdbLogo from '../Resources/imdb.png'
+
+function formatMoney(n) {
+    return "$ " + (Math.round(n * 100) / 100).toLocaleString();
+}
+
+function formatTime(n) {
+    var num = n;
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return rhours + "h " + rminutes + "mins";
+}
 
 export default function MovieDetails({key}) {
     const [movie, setMovie] = useState();
@@ -24,15 +38,21 @@ export default function MovieDetails({key}) {
     if (!movie) {
         return <div />
     }
+    console.log('movie', movie)
     return (
-        <div>
-            <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} />
-            <h1>{movie.original_title}</h1>
-            <h6>{movie.release_date}</h6>
-            <h6>Movie budget: {movie.budget}</h6>
-            <h5>{movie.tagline}</h5>
-            <div>{movie.tagline}</div>
+        <div className='flex-container'>
+            <div className='flex-container-row'>
+                <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title}/>
+                <div className='movie-details-header'>
+                    <h1>{movie.original_title} ({movie.release_date.substring(0,4)})</h1>
+                    <h5>Genres: {movie.genres.map(genre => genre.name + ' / ')}</h5>
+                    <h5>Budget: {formatMoney(movie.budget)}</h5>
+                    <h5>Revenue: {formatMoney(movie.revenue)}</h5>
+                    <h5>Runtime: {formatTime(movie.runtime)}</h5>
+                    <div>{movie.production_companies.map(company => <img src={`https://image.tmdb.org/t/p/h60${company.logo_path}`}  width='40px' alt={company.name} key={company.name}/>)}</div>
+                    <a href={`https://www.imdb.com/title/${movie.imdb_id}`} target="_blank"><img src={imdbLogo} alt='imdb' width='40px'/></a>
+                </div>
+            </div>
             <div>{movie.overview}</div>
-        </div>
-        );
-}
+        </div>)
+    }
--- src/Components/searchAppBar.js
+++ src/Components/searchAppBar.js
@@ -104,7 +104,7 @@ export default function SearchAppBar() {
               <MenuIcon />
             </IconButton>
             <Typography className={classes.title} variant="h6" noWrap>
-              React Movie Search
+              Movie Search
             </Typography>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
